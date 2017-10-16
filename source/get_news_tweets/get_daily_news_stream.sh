0f258399932ca5dc117a45c846211de5edd06223
vered1986 <vered1986@gmail.com>
2017-10-16 14:29:18 +0300

    update resource; #instances: 3972730; #rules: 1328561
    Former-commit-id: e138c4f829b7be407b3c0e1fba0ceb0505d98af5 [formerly 5728eb28465df1123fa11687684830b0737c4c4b] [formerly e9756a03a078e2262b876a3ffb957d98cce87081 [formerly 0bc7643b734c0a2cd4c43e0d961b0f406e35768f]]
    Former-commit-id: 126b396034599b6ffde5fa605956f6a9aaea865e [formerly 6e5601c500e4fe12e3cf2d2500ebf4313d3b000c]
    Former-commit-id: c011beefa2027be286e239941fa88b5027fe7b7a

--- source/get_news_tweets/get_daily_news_stream.sh
+++ source/get_news_tweets/get_daily_news_stream.sh
@@ -15,15 +15,20 @@ while true; do
   pkill -f get_news_tweets_stream.py;
   python -u get_news_tweets_stream.py $consumer_key $consumer_secret $access_token $access_token_secret &
   
-  # Get propositions and positive instances for the previous day, package and release a new version of the resource
+  # Get propositions and positive instances for the previous day, 
   last_file=`date -d "yesterday" '+%Y_%m_%d'`;
   (python -u prop_extraction.py --in=news_stream/tweets/$last_file --out=news_stream/props/$last_file.prop > prop.log;
   python -u get_corefering_predicates.py news_stream/props/$last_file.prop news_stream/positive/$last_file;
+  
+  # Package and release a new version of the resource
   cat news_stream/positive/* | cut -f1,2,4,5,6,7,8,10,11,12,13,14 > resource;
   python -u package_resource.py resource resource_dir;
   zip resource_dir/resource.zip resource_dir/*.tsv;
   python upload_to_dropbox.py $dropbox_access_token resource_dir;
-  cp resource_dir/resource.zip $repository_dir/resource;
+  
+  # Update the repository with statistics
+  wc -l resource_dir/instances.tsv > $repository_dir/resource/curr_stats
+  wc -l resource_dir/rules.tsv >> $repository_dir/resource/curr_stats
   git --git-dir=$repository_dir/.git pull;
   git --git-dir=$repository_dir/.git --work-tree=$repository_dir/ commit -m "update resource; #instances: `wc -l instances.tsv`; #rules: `wc -l rules.tsv`" resource/*;
   git --git-dir=$repository_dir/.git --work-tree=$repository_dir/ push origin master) &
